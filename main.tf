59f58174027e704b4c7f4c8cda89f4b7e7dd1946
Nitin Kansal <kansalster@gmail.com>
2021-02-26 11:37:39 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,10 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.46"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+resource "random_string" "random" {
+  length           = 5
+  special          = false
+  lower            = true
+  upper            = false
+}
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "storageaccount${random_string.random.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
@@ -14,3 +34,8 @@ resource "azurerm_storage_account" "example" {
     environment = "staging"
   }
 }
+
+
+output "storage_account" {
+  value = azurerm_storage_account.example.name
+ }
